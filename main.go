78f65e280cd3347d62eab3027402902a41f3a4c0
gosmartwizard <gosmartwizard@gmail.com>
2023-02-02 19:18:14 +0530

    Examining the http.Request type

--- main.go
+++ main.go
@@ -19,10 +19,15 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:nk@smartwizard.io\">nk@smartwizard.io</a>.</p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
+}
+
 func main() {
 
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	//http.HandleFunc("/", homeHandler)
+	//http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 
 	fmt.Println("Starting the server on :3000...")
 
