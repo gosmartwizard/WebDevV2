b9d9d50b37e92284126d98db16243b068ea91636
gosmartwizard <gosmartwizard@gmail.com>
2023-02-07 09:59:40 +0530

    Embedding Template files

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi"
 	"github.com/gosmartwizard/WebDevV2/controllers"
+	"github.com/gosmartwizard/WebDevV2/templates"
 	"github.com/gosmartwizard/WebDevV2/views"
 )
 
@@ -14,15 +14,15 @@ func main() {
 
 	r := chi.NewRouter()
 
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	tpl, err := views.ParseFS(templates.FS, "home.gohtml")
 	views.Must(tpl, err)
 	r.Get("/", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	tpl, err = views.ParseFS(templates.FS, "contact.gohtml")
 	views.Must(tpl, err)
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	tpl, err = views.ParseFS(templates.FS, "faq.gohtml")
 	views.Must(tpl, err)
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
